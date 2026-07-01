87460e6073168814f5fe21f09c786c2b25fdc43e
shahsrushti22 <shahsrushti2202@gmail.com>
2026-07-01 15:14:59 +0530

    Add const variable examples with explanations for constant behavior

---
+++ js/variables/const.js
@@ -0,0 +1,11 @@
+const d=10;
+console.log(d);
+// const d=20; // This will throw an error because 'd' has already been declared as a constant.
+//we cant asign the value to the constant variable again.//
+//we can access value many time but cant use again the variable name to assign the value.//
+//we cant change the value in const variable like we can change in let and var variable.//
+//d=5;//
+console.log(d); // This will throw an error because we cannot reassign a value to a constant variable.//
+const p=10;
+console.log(p);
+
