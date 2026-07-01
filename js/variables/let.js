59622ee653b94626ad13121ef472243a9f5ad0dd
shahsrushti22 <shahsrushti2202@gmail.com>
2026-07-01 12:32:16 +0530

    multiple classes in one let and underscore

--- js/variables/let.js
+++ js/variables/let.js
@@ -1,11 +1,24 @@
-let n=10;
-console.log(n)
+/*let n=10;
+console.log(n)*/
 /*let n=20;
 console.log(n)*/
 /*it will give the error because we have used same variable 2 times so 
 we can change the value of the variable but canr use variable twice it will give the error if you are using the let key to create the variable
 eg. is above */
-n=20;
-console.log(n)
+/*n=20;
+console.log(n)*/
 /*we have changed the value n=20; in above eg now we wlll get the output of the n is 20*/
 
+/*{
+    var p=30;
+    let a=20;
+}*/
+/*console.log(p)
+console.log(a)*/
+/*it will give the error because we have used let key to create the variable a and it is in block scope so we can not access it outside the block but var key is in global scope so we can access it outside the block*/
+/*we cant give the space in the variable name and also we cant use the special character in the variable name but we can use the underscore(_) and dollar($) sign in the variable name eg. let _a=20; let $a=20;*/
+/*let abc_hello = 10;
+console.log(abc_hello) */
+let s=20 , r=30, u=40;
+console.log(s,r,u)
+/*we can declare the multiple variable in single line using let key eg. is above*/
