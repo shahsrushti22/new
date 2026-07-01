643e095d48eaec77e5afa08c54194d57c2e2e56a
shahsrushti22 <shahsrushti2202@gmail.com>
2026-07-01 11:37:28 +0530

    var and let>

---
+++ js/variables/index.js
@@ -0,0 +1,11 @@
+var n=10;
+console.log(n)
+console.log("n")
+var m="hello"
+console.log(m)
+n=50;
+console.log(n)
+n="ws"
+console.log(n)
+var n="hello world"
+console.log(n)
---
+++ js/variables/let.js
@@ -0,0 +1,10 @@
+let n=10;
+console.log(n)
+/*let n=20;
+console.log(n)*/
+/*it will give the error because we have used same variable 2 times so 
+we can change the value of the variable but canr use variable twice it will give the error if you are using the let key to create the variable
+eg. is above */
+n=20;
+console.log(n)
+/*we have changed the value n=20; in above eg now we wlll get the output of the n is 20*/
