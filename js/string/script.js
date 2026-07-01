75d79cf1cf5d7b09e092f1717c339e0c4cedcfcf
shahsrushti22 <shahsrushti2202@gmail.com>
2026-07-01 15:14:30 +0530

    Add string and template literals example to demonstrate dynamic content creation

---
+++ js/string/script.js
@@ -0,0 +1,9 @@
+//string and template literals is a way to create strings in JavaScript. Template literals allow for multi-line strings and string interpolation, making it easier to work with dynamic content.//
+let n=10
+let w=`welcome ${n} to js coding`
+console.log(w)
+//template literals is a process where we can use backticks(``) to create strings and embed expressions inside them using ${}. This allows for easier string concatenation and formatting.//
+let p=20
+let c=`welcome ${p} to ${10+20}js coding`
+console.log(c)
+//so we can use template literals to create dynamic strings that can include variables and expressions, making our code more readable and maintainable.//
